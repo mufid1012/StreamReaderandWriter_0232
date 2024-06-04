38b50719d58d7b34de3f19eb848808ba468dcef2
mufid1012 <mufidghibran1012@gmail.com>
2024-06-04 15:13:46 +0700

    menambahkan fungsi catch

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,4 +5,14 @@
 //untuk obyek array yang akan digunakan
 using namespace std;
 
-
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 3, 5 ,7 };
+        //pesan array interger 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+   
+}
